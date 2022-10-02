7170ff0d02dbb47d85f8b50769cd0b70b44b1829
AragornKnight007 <zchoudhary37@gmail.com>
2022-10-02 10:04:34 +0530

    Complete TODO App Based On MVVM Architecture In Android Studio

---
+++ app/src/main/java/com/example/mynotemvvm/DataInsertActivity.java
@@ -0,0 +1,72 @@
+package com.example.mynotemvvm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mynotemvvm.databinding.ActivityDataInsertBinding;
+
+public class DataInsertActivity extends AppCompatActivity {
+
+    ActivityDataInsertBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        binding = ActivityDataInsertBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        String type = getIntent().getStringExtra("type");
+        if (type.equals("update")) {
+            setTitle("update");
+
+            binding.title.setText(getIntent().getStringExtra("title"));
+            binding.disp.setText(getIntent().getStringExtra("disp"));
+            int id = getIntent().getIntExtra("id", 0);
+
+            binding.add.setText("update note");
+            binding.add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.putExtra("title", binding.title.getText().toString());
+                    intent.putExtra("disp", binding.disp.getText().toString());
+                    intent.putExtra("id", id);
+                    setResult(RESULT_OK, intent);
+                    finish();
+
+                }
+            });
+
+
+        } else {
+            setTitle("Add Mode");
+
+            binding.add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    intent.putExtra("title", binding.title.getText().toString());
+                    intent.putExtra("disp", binding.disp.getText().toString());
+                    setResult(RESULT_OK, intent);
+                    finish();
+
+                }
+            });
+
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(DataInsertActivity.this, MainActivity.class));
+    }
+}
